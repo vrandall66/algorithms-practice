2ae6306854f640be434d82fc3eb8b919275004fa
Vanessa Randall <vanessalrandall@gmail.com>
2020-01-29 14:19:05 -0700

    Initial commit

---
+++ maxchar/index.js
@@ -0,0 +1,31 @@
+// --- Directions
+// Given a string, return the character that is most
+// commonly used in the string.
+// --- Examples
+// maxChar("abcccccccd") === "c"
+// maxChar("apple 1231111") === "1"
+
+function maxChar(str) {
+  // return str.split('').reduce((acc, cur) => {
+  //   return acc[char] ? acc[char] = 1 : acc[char]++
+  // }, {})
+  // NOT SOLVED ALL THE WAY
+
+  const charMap = {};
+  let max = 0;
+  let maxChar = '';
+
+  for (let char of str) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
+}
+
+module.exports = maxChar;
